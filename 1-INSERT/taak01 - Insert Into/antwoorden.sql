30b20cf0aa6e07553a10d1b955c22ac76bff0320
Abdelrahmanroc <90619774+Abdelrahmanroc@users.noreply.github.com>
2022-02-25 16:34:54 +0100

    yes

--- 1-INSERT/taak01 - Insert Into/antwoorden.sql
+++ 1-INSERT/taak01 - Insert Into/antwoorden.sql
@@ -1,1 +1,13 @@
+INSERT INTO studenten (student_id, voornaam, achternaam, klas, adres, postcode, geboortedatum, woonplaats)
+VALUES (1, 'Emma', 'da silva', '9A', 'Albert Cuypstraat 3', '1234AB', '2001-04-28', 'Amstelveen');
+--------------------------------------------------------------------------------------------------
+INSERT INTO studenten (student_id, voornaam, achternaam, klas, adres, postcode, geboortedatum, woonplaats)
+VALUES (6, 'Mohammed', 'Atari', '9C', 'Kalverstraat 1', '1678DC', '2003-01-17', 'Amstelveen');
+---------------------------------------------------------------------------------------------------
+INSERT INTO studenten (student_id, voornaam, achternaam, klas, adres, postcode, geboortedatum, woonplaats)
+VALUES (5, 'Abdalla', 'Atari', '1C', 'Kalverstraat 2', '1278DC', '2004-02-18', 'Amstelveen');
+---------------------------------------------------------------------------------------------------
+INSERT INTO studenten (student_id, voornaam, achternaam, klas, adres, postcode, geboortedatum, woonplaats)
+VALUES (7, 'Karim', 'Atari', '8C', 'Kalverstraat 3', '1878DC', '2005-04-19', 'Amstelveen');
+--------------------------------------------------------------------------------------------------
 
