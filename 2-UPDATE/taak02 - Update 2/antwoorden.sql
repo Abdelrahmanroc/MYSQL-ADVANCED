591aee6ca609e8a3bf7d37d45f86d4451573bf1e
Abdelrahmanroc <90619774+Abdelrahmanroc@users.noreply.github.com>
2022-02-28 13:24:09 +0100

    update

--- 2-UPDATE/taak02 - Update 2/antwoorden.sql
+++ 2-UPDATE/taak02 - Update 2/antwoorden.sql
@@ -1,14 +1,9 @@
 -- Opdracht 1
-UPDATE studenten2 SET woonplaats = 'Amstelveen';
+
 -- Opdracht 2 
-1- UPDATE studenten2 SET adres = 'Albert dago22', postcode= '1215MZ' WHERE student_id = 8;
-2- UPDATE studenten2 SET adres = 'Albert dago23', postcode= '1215M1' WHERE student_id = 9;
-3- UPDATE studenten2 SET adres = 'Albert dago24', postcode= '1215M2' WHERE student_id = 10;
+
 -- Opdracht 3
-UPDATE studenten2 SET geboortedatum ='2000-12-12' WHERE student_id = 2;
+
 -- Opdracht 4
-UPDATE studenten2 SET klas ='9C' WHERE student_id = 4;
-UPDATE studenten2 SET klas ='9A' WHERE student_id = 8;
---Opdracht 5
-UPDATE studenten2 SET voornaam ='Johannah' WHERE student_id = 6;
+
 
